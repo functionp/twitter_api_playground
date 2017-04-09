8fe0fe27e9805cc70ad004f0faff17856da34924
hiroki <functionp@gmail.com>
2017-04-09 18:40:53 +0900

    initial commit

---
+++ search_api.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import requests_oauthlib
+import requests
+import json
+
+
+token = {'TOKEN': "",
+         'TOKEN_SECRET': "",
+         'CONSUMER_KEY': "",
+         'CONSUMER_SECRET': ""}
+
+
+header_oauth = requests_oauthlib.OAuth1(token['CONSUMER_KEY'],
+                                        token['CONSUMER_SECRET'],
+                                        token['TOKEN'],
+                                        token['TOKEN_SECRET'],
+                                        signature_type='auth_header')
+
+url = 'https://api.twitter.com/1.1/search/tweets.json?q=superbowl&result_type=recent'
+response = requests.get(url, auth=header_oauth)
+
+tweets = json.loads(response.text)['statuses']
+
+for tweet in tweets:
+    print('==================')
+    print(tweet['text'])
+    #print(tweet['screen_name'])
+    print(tweet['created_at'])
+    print(tweet['id'])
+    print(tweet['id'])
